bcf22ad8d3829e17e5cc41f085e0fbbdf4c7aa86
Ivan Konishchev <kispaj@mail.ru>
2022-11-11 17:29:43 +0300

    Create swiftUILesson5 project

---
+++ swiftUILesson5/swiftUILesson5App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUILesson5App.swift
+//  swiftUILesson5
+//
+//  Created by Ivan Konishchev on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUILesson5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
